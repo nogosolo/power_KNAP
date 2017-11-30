c319d50339d5228f8a8e8de038ac5be8b8ff7a9c
Carrie Zhang <carrie.zhang8206@gmail.com>
2017-11-30 12:31:58 -0800

    add FBLogin as react component

--- client/src/components/Main/SiteNavBar.jsx
+++ client/src/components/Main/SiteNavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FBLogin from './FBLogin';
 // import LoginButton from './LoginButton';
 // import SearchBox from './SearchBox';
 
@@ -9,8 +10,9 @@ const SiteNavBar = (props) => (
   <a class="active"><Link to="/" >Home</Link></a>
   <a ><Link to="/rooms">Room</Link></a>
   <a ><Link to="/javi">JAVI LINK</Link></a>
+  <a ><FBLogin /></a>
 </div>
-        <span className="login">Welcome, put state here <a href="/auth/logout">Logout</a></span>
+      <span className="login">Welcome, put state here <a href="/auth/logout">Logout</a></span>
       <span className="login">Login with <a href="/auth/google">Google</a></span>
 </div>
 )
