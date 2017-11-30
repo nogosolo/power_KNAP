e53b388cd6d2aead479963d3adb4dc97a27da718
javi <enriq018coding@gmail.com>
2017-11-29 22:49:35 -0800

    browse layout

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -8,11 +8,21 @@ import Sidebar from './Sidebar.jsx';
 //
 // const lobby = io('/lobby');
 
+const PreviewRoom = (props) => (
+  <div clasName="panel" onClick={()=> console.log('clicked!')}>
+    <div className="op">
+      <img  className={'pic5'} src={'https://static.pexels.com/photos/110854/pexels-photo-110854.jpeg'}/> 
+      <h1>Room Number {props.ex} </h1>  
+    </div>
+  </div>
+
+)
+
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      roomList: ['Room1', 'Room2', 'Room3'],
+      roomList: [1,2,3,4,5,6,7,8],
     };
   }
 
@@ -23,18 +33,11 @@ class Homepage extends React.Component {
     return (
       <div>
         <h1>Browse Rooms</h1>  
-        <div class="wrapper">
-          <div class="panel">Panel A</div>
-          <div class="panel">Panel B</div>
-          <div class="panel">Panel C</div>
-          <div class="panel">Panel D</div>
-          <div class="panel">Panel E</div>
-          <div class="panel">Panel F</div>
-          <div class="panel">Panel G</div>
-          <div class="panel">Panel H</div>
-          <div class="panel">Panel I</div>
-          <div class="panel">Panel J</div>
-        </div>
+          <div className="wrapper">
+            {this.state.roomList.map((el,key) => <PreviewRoom ex={el} />)}
+            
+
+          </div>
       </div>
     );
   }
