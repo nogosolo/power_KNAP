8112d942b8c61293127041953041a55d5e2bcbb1
Carrie Zhang <carrie.zhang8206@gmail.com>
2017-11-29 19:34:25 -0800

    integrade facebook login, and get check status working, also add a login button to navigate/pop-up fb login

--- database/postgres.js
+++ database/postgres.js
@@ -28,10 +28,9 @@ const Room = sequelize.define('room', {
 
 // Video.sync({ force: true });
 // Room.sync({ force: true });
-<<<<<<< HEAD
+
 // Playlist.sync({ force: true });
-=======
->>>>>>> add fb script to index.html
+
 
 const createVideoEntry = (videoData) => {
   const videoEntry = {
