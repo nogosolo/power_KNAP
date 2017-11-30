817db7fa29d19e56ed52c37cddcafee1e1c61ed4
Brian Pak <brianpak27@gmail.com>
2017-11-29 17:31:41 -0800

    roomlist mapping in sidebar

--- client/src/components/Homepage/Sidebar.jsx
+++ client/src/components/Homepage/Sidebar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import RoomListEntry from './RoomListEntry.jsx';
 
-const Sidebar = () => (
+const Sidebar = (props) => (
   <div>
-    <p>Sidebar</p>
+    <h3>Sidebar</h3>
+    {props.rooms.map((room, idx) => (<RoomListEntry room={room} key={idx}/>))}
   </div>
 );
 
