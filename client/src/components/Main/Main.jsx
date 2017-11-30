0fab1500c33e9e6b12cf1f710b38e229913abcfa
Brian Pak <brianpak27@gmail.com>
2017-11-29 16:30:56 -0800

    pulled in react router changes

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 // import SignupPage from './SignupPage';
 // import SearchPage from './SearchPage';
 // import MatchPage from './MatchPage';
-import HomePage from '../Homepage/Homepage';
+import HomePage from '../Homepage/homepage';
 import RoomView from '../Room/RoomView';
 import Javi from '../Homepage/Javi';
 
