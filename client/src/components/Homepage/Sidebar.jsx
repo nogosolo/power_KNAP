1ad062896fcf6d77156e002d75d53b3426208484
Javier Enriquez <enriq018coding@gmail.com>
2017-11-30 16:15:42 -0800

    Update Sidebar.jsx

--- client/src/components/Homepage/Sidebar.jsx
+++ client/src/components/Homepage/Sidebar.jsx
@@ -1,11 +1,1 @@
-import React from 'react';
-import RoomListEntry from './RoomListEntry.jsx';
 
-const Sidebar = (props) => (
-  <div>
-    <h3>Sidebar</h3>
-    {props.rooms.map((room, idx) => (<RoomListEntry room={room} key={idx}/>))}
-  </div>
-);
-
-export default Sidebar;
