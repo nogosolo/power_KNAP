24cfbd0f97ec7f1a7644562b2823cd09f7819bd7
Carrie Zhang <carrie.zhang8206@gmail.com>
2017-11-30 19:29:27 -0800

    implement FB login in ReactJS compomnetDidMount

---
+++ client/src/components/Main/FBLoginBtn.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+
+class FBLoginBtn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+   window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '168772223720117',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.11'
+      });
+
+    FB.getLoginStatus(function(response) {
+      this.statusChangeCallback(response);
+    }.bind(this));
+  }.bind(this);
+
+  // Load the SDK asynchronously
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=168772223720117';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+}
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+  testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      // document.getElementById('status').innerHTML =
+      // 'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+
+// This is called with the results from from FB.getLoginStatus().
+  statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      this.testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
+    }
+  }
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+  checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      this.statusChangeCallback(response);
+    }.bind(this));
+  }
+
+  handleClick() {
+    FB.login(this.checkLoginState());
+  }
+
+  render() {
+    return(
+      <div id="fb-root">
+        <div class="fb-login-button" 
+        data-max-rows="1" 
+        data-size="large" 
+        data-button-type="login_with" 
+        data-show-faces="true" 
+        data-auto-logout-link="true" 
+        data-use-continue-as="true"
+        onClick={this.handleClick}>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FBLoginBtn;
+// const FBLogin = (props) => (
+
+//   <div id="fb-root">
+//     <script>{(function(d, s, id) {
+//       var js, fjs = d.getElementsByTagName(s)[0];
+//       if (d.getElementById(id)) return;
+//       js = d.createElement(s); js.id = id;
+//       js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=168772223720117';
+//       fjs.parentNode.insertBefore(js, fjs);
+//     }(document, 'script', 'facebook-jssdk'))}</script>
+
+//     <div class="fb-login-button" data-max-rows="1" data-size="large" data-button-type="login_with" data-show-faces="false" data-auto-logout-link="true" data-use-continue-as="true" onlogin={this.checkLoginState}></div>
+//     <div id="status"></div>
+//   </div>
+// )
+        // <script>{(function(d, s, id) {
+        //   var js, fjs = d.getElementsByTagName(s)[0];
+        //   if (d.getElementById(id)) return;
+        //   js = d.createElement(s); js.id = id;
+        //   js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=168772223720117';
+        //   fjs.parentNode.insertBefore(js, fjs);
+        // }(document, 'script', 'facebook-jssdk'))}</script>
