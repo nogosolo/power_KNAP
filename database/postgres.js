da86566fc2d0d7007d32e8ac17265ea2d98b2ef3
RudyRed <willgross94@gmail.com>
2017-12-02 11:41:03 -0800

    fixed some double adding cases

--- database/postgres.js
+++ database/postgres.js
@@ -83,7 +83,7 @@ const createRoom = (roomName, cb) => {
 }
 
 // Video Queries
-const findVideos = roomId => sequelize.query(`select distinct on ("createdAt") * from videos where "roomId" = ${roomId}`, { type: sequelize.QueryTypes.SELECT});
+const findVideos = roomId => sequelize.query(`select distinct on (date_trunc('second', "createdAt")) * from videos where "roomId" = ${roomId}`, { type: sequelize.QueryTypes.SELECT});
 const removeFromPlaylist = (title, roomId, createdTime) => Videos.findAll({ where: { videoName: title, roomId: roomId , createdAt: createdTime} }).then(video => video.destroy());
 
 exports.createVideoEntry = createVideoEntry;
